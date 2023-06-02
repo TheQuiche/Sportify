1fd437e0b225266c480363310a1a7330446cb085
TheQuiche <104493145+TheQuiche@users.noreply.github.com>
2023-06-02 15:40:10 +0200

    Add files via upload

---
+++ traitement_connexion_coach.php
@@ -0,0 +1,39 @@
+<?php
+// Récupérer les données du formulaire
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $Email = $_POST["email"];
+    $mdp = $_POST["password"];
+
+    // Connexion à la base de données
+    $servername = "localhost";
+    $username = "root";
+    $password_db = "";
+    $dbname = "connexion";
+
+    $conn = new mysqli($servername, $username, $password_db, $dbname);
+
+    // Vérifier si la connexion a échoué
+    if ($conn->connect_error) {
+        die("Erreur de connexion à la base de données : " . $conn->connect_error);
+    }
+
+    // Échapper les données pour éviter les attaques par injection SQL
+    $Email = mysqli_real_escape_string($conn, $Email);
+    $mdp = mysqli_real_escape_string($conn, $mdp);
+
+    // Requête SQL pour vérifier les données du client
+    $sql = "SELECT * FROM coach WHERE Email='$Email' AND mdp='$mdp'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // Le client existe dans la base de données
+        header("Location: profil_coach.html"); // Redirection vers une autre page HTML
+        exit(); // Terminer le script pour éviter toute exécution supplémentaire
+    } else {
+        // Le client n'existe pas ou les informations d'identification sont incorrectes
+        echo "Identifiants invalides!";
+    }
+
+    $conn->close();
+}
+?>
